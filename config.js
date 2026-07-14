cffe608a08900175ca096dfc6d53df250e6350af
suads <timirkosarev@gmail.com>
2026-07-15 01:49:20 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+ apiKey: "AIzaSyAxo4VkVVd8PqGJdM5ncIC-W8mQvU0z9uI",
+  authDomain: "grt4r-7758d.firebaseapp.com",
+  projectId: "grt4r-7758d",
+  storageBucket: "grt4r-7758d.firebasestorage.app",
+  messagingSenderId: "1044260841178",
+  appId: "1:1044260841178:web:271150cdbb31a90794e7e3",
+  measurementId: "G-5GY6WG5PLT"
+};
+ 
